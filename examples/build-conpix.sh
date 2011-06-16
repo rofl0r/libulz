5c3121ec7c054356505f6e237c6a27d08c1474f5
rofl0r <retnyg@gmx.net>
2011-06-16 02:10:04 +0200

    removed separate console library

--- examples/build-conpix.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#musl-gcc console_conpix.c ../console.c -I.. -I/usr/local/musl/include/ncursesw/ -llept -lz -lpng -ltiff -ljpeg -lncursesw -ltinfow -lticw
-gcc -Wall -Wextra -g console_conpix.c ../console.c -I.. -lncurses -llept -o console_conpix
--- examples/build-contest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#musl-gcc console_test.c console.c -I /usr/local/musl/include/ncursesw/ -lpng -llept -lncursesw -ltinfow -lticw
-gcc -Wall -Wextra -g console_test.c ../console.c -I.. -lncurses -o console_test
