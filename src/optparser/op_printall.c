dd2b87f1bb06af22b7ced61c5760853a1752bbdb
rofl0r <retnyg@gmx.net>
2011-07-27 02:59:18 +0200

    use proper fd for stdout

--- src/optparser/op_printall.c
+++ src/optparser/op_printall.c
@@ -5,10 +5,10 @@ void op_printall(opts* options) {
 	size_t i;
 	if (!options || !options->options || !options->options->size) return;
 	for(i = 0; i < options->options->size/2; i++)
-		log_put(0, VARIS(stringptrlist_get(options->options, (i*2))), VARISL(": "), VARIS(stringptrlist_get(options->options, (i*2 + 1))), NULL);
+		log_put(1, VARIS(stringptrlist_get(options->options, (i*2))), VARISL(": "), VARIS(stringptrlist_get(options->options, (i*2 + 1))), NULL);
 	if(options->flags && options->flags->size && options->flags->ptr) {
-		log_puts(0, options->flags);
-		log_putln(0);
+		log_puts(1, options->flags);
+		log_putln(1);
 	}
 }
 
