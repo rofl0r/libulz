50914804ee377d685569fd80769d523f9d48bce8
rofl0r <retnyg@gmx.net>
2011-07-28 14:43:15 +0200

    switch bitarray to macro-only

--- include/bitarray.h
+++ include/bitarray.h
@@ -11,13 +11,18 @@
 #define _BITARRAY_H_
 
 #include <stddef.h>
+#include <limits.h>
 
-typedef char bitarray;
-
-bitarray* ba_new(size_t size);
-void ba_set(bitarray* ba, size_t bit, char value);
-char ba_get(bitarray* ba, size_t bit);
-
+#define BA_SIZE_REQUIRED(X) (((X) / CHAR_BIT) + !!((X) % CHAR_BIT))
+#define BA_TARGET_BYTE(X, Y) ((X) + ((Y) / CHAR_BIT))
+#define BA_BIT_DISTANCE(X, Y) ((Y) % CHAR_BIT)
+#define BA_GET(X, Y) (!!( *BA_TARGET_BYTE(X, Y) & (1 << BA_BIT_DISTANCE(X, Y)) ))
+#define BA_SET_TRUE(X, Y) do { \
+			*(BA_TARGET_BYTE(X, Y)) |= (1 << BA_BIT_DISTANCE(X, Y)); \
+			} while(0);
+#define BA_SET_FALSE(X, Y) do { \
+			*(BA_TARGET_BYTE(X, Y)) &= ~(1 << BA_BIT_DISTANCE(X, Y)); \
+			} while(0);
+			
 #endif
 
-//RcB: DEP "../src/bitarray/bitarray.c"
