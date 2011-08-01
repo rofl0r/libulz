897208da0c6fb5c8434a1d80e7c2deabf6e4ecd1
rofl0r <retnyg@gmx.net>
2011-08-01 08:04:09 +0200

    be more consistent in getpathdiff

--- src/filelib/getpathdiff.c
+++ src/filelib/getpathdiff.c
@@ -6,9 +6,16 @@
 // additionally a zero delimiter is placed in workbuf.
 // the size of the difference is returned as result, and -1 on failure.
 // workbuf should be at least as big as path2, but not longer than PATH_MAX.
+// all returned results start with /
 ssize_t getpathdiff(stringptr* path1, stringptr* path2, char* workbuf) {
 	normalizepath(path1);
 	normalizepath(path2);
+	// special case : / is the only case where the end of the string is /
+	int isRoot = 0;
+	if(path1->size == 1) {
+		path1->size = 0;
+		isRoot = 1;
+	}
 	ssize_t result = path2->size - path1->size;
 	if(result < 0) return -1;
 	if(result) {
@@ -16,5 +23,6 @@ ssize_t getpathdiff(stringptr* path1, stringptr* path2, char* workbuf) {
 		memcpy(workbuf, path2->ptr + path1->size, result);
 	}
 	workbuf[result] = 0;
+	if(isRoot) path1->size = 1;
 	return result;
 }
