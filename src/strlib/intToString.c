ec6e74d21a354424813049e541a2a7e3aae6ebee
rofl0r <retnyg@gmx.net>
2011-08-01 08:05:25 +0200

    remove unwished padding

--- src/strlib/intToString.c
+++ src/strlib/intToString.c
@@ -1,7 +1,7 @@
 #include "../../include/strlib.h"
 
-
-char* intToString(int number, char* buffer, size_t bufsize) {
-	if (!buffer || !bufsize) return NULL;
-	return numberToString((int64_t) number, 10, buffer, bufsize, NTS_SIGNED_TYPE);
+// it is in the responsibility of the called to deliver a buffer of suitable length.
+char* intToString(int number, char* buffer) {
+	if (!buffer) return NULL;
+	return numberToString((int64_t) number, 10, buffer, 0, NTS_SIGNED_TYPE);
 }
