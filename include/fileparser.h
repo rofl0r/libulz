60c88c318085d073d9d283c15ead8c26ce6cf45d
rofl0r <retnyg@gmx.net>
2011-08-01 15:14:41 +0200

    added capability to retrieve over-sized lines from fileparser

--- include/fileparser.h
+++ include/fileparser.h
@@ -15,12 +15,16 @@ typedef struct {
 
 // success: 0
 int fileparser_open(fileparser* parser, char* filename);
-// success: 0
+// success:0, buffer to small: -3
 int fileparser_readline(fileparser* parser);
 // success: 0
 int fileparser_close(fileparser* parser);
 // success: 0
 int fileparser_getline(fileparser* parser, stringptr* line);
+// success: size till next nl
+ssize_t fileparser_getnextlinelength(fileparser* parser);
+// success:0, buffer to small: -3
+int fileparser_readline_userbuf(fileparser* parser, char* buf, size_t bufsize);
 
 #endif
 
--- src/fileparser/fileparser.c
+++ src/fileparser/fileparser.c
@@ -13,19 +13,20 @@ int fileparser_open(fileparser* parser, char* filename) {
 }
 
 // success: 0
-int fileparser_readline(fileparser* parser) {
+// -3: buffer to small
+int fileparser_readline_userbuf(fileparser* parser, char* buf, size_t bufsize) {
 	size_t i;
 	size_t found = 0;
 	ssize_t ret;
 	if(!parser || !parser->file) return -1;
-	if(!(ret = read(parser->file, parser->buf, sizeof(parser->buf)))) return -2;
+	if(!(ret = read(parser->file, buf, bufsize))) return -2;
 	if(ret == -1) return errno;
 	parser->len = (size_t) ret;
 	for(i = 0; i < parser->len; i++) {
-		if(parser->buf[i] == '\n') {
+		if(buf[i] == '\n') {
 			found = 1;
 			nl:
-			parser->buf[i] = 0;
+			buf[i] = 0;
 			parser->fpos += i + 1;
 			parser->len = i;
 			if(found && lseek(parser->file, (off_t) parser->fpos, SEEK_SET) == -1) return errno;
@@ -33,11 +34,35 @@ int fileparser_readline(fileparser* parser) {
 		}
 	}
 	// this covers the case when the file doesnt end with \n
-	if(parser->len < sizeof(parser->buf)) {
+	if(parser->len < bufsize) {
 		i = parser->len;
 		goto nl;
 	}
+	// too big for our buffer, reset fp to last position.
+	if(lseek(parser->file, (off_t) parser->fpos, SEEK_SET) == -1) return errno;
 	return -3;
+}
+// success: 0
+// -3: buffer to small
+int fileparser_readline(fileparser* parser) {
+	return fileparser_readline_userbuf(parser, parser->buf, sizeof(parser->buf));
+}
+
+// returns length of next line, or -1 on failure
+ssize_t fileparser_getnextlinelength(fileparser* parser) {
+	size_t res = 0;
+	uint64_t store_fpos = parser->fpos;
+	int ret;
+	while((ret = fileparser_readline(parser)) == -3) {
+		res += sizeof(parser->buf);
+		parser->fpos += sizeof(parser->buf);
+		if(lseek(parser->file, (off_t) parser->fpos, SEEK_SET) == -1) return -1;
+	}
+	if(ret != 0) return -1;
+	res += parser->len;
+	parser->fpos = store_fpos;
+	if(lseek(parser->file, (off_t) parser->fpos, SEEK_SET) == -1) return -1;
+	return res;
 }
 
 // success: 0
