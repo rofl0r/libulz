7d7fd2dc59d261b0adfcb27f05f03912c54b105b
rofl0r <retnyg@gmx.net>
2011-08-28 12:21:01 +0200

    fix timestamp formatstring. never commit before testing...

--- src/timelib/timestamp.c
+++ src/timelib/timestamp.c
@@ -8,5 +8,5 @@ size_t timestamp(char* buffer, size_t bufsize) {
 	time_t secs = time(NULL);
 	struct tm tim;
 	localtime_r(&secs, &tim);
-	return ulz_snprintf(buffer, bufsize, "%.4d.%.2d.%.2d %.2d:%.2d.%:2d", 1900 + tim.tm_year, tim.tm_mon, tim.tm_mday, tim.tm_hour, tim.tm_min, tim.tm_sec);
+	return ulz_snprintf(buffer, bufsize, "%.4d.%.2d.%.2d %.2d:%.2d:%.2d", 1900 + tim.tm_year, tim.tm_mon, tim.tm_mday, tim.tm_hour, tim.tm_min, tim.tm_sec);
 }
