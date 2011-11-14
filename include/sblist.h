73a5fe2b81cbd9c24ae1d0714d9f4da08cd7b46b
rofl0r <retnyg@gmx.net>
2011-11-14 01:01:00 +0100

    added iterator macros to sblist

--- include/sblist.h
+++ include/sblist.h
@@ -20,6 +20,7 @@ typedef struct {
 } sblist;
 
 #define sblist_getsize(X) ((X)->count)
+#define sblist_get_count(X) ((X)->count)
 
 // for dynamic style
 sblist* sblist_new(size_t itemsize, size_t blockitems);
@@ -34,7 +35,19 @@ void* sblist_get(sblist* l, size_t item);
 // returns 1 on success, 0 on OOM
 int sblist_add(sblist* l, void* item);
 int sblist_set(sblist* l, void* item, size_t pos);
-#define sblist_get_count(X) ((X)->count)
+void sblist_delete(sblist* l, size_t item);
+
+
+#define __sblist_concat_impl( x, y ) x##y
+#define __sblist_macro_concat( x, y ) __sblist_concat_impl( x, y )
+#define __sblist_iterator_name __sblist_macro_concat(sblist_iterator, __COUNTER__)
+
+// use with custom iterator variable
+#define sblist_iter_counter(LIST, ITER, PTR) size_t ITER; \
+	for(ITER = 0; (PTR = sblist_get(LIST, ITER)), ITER < sblist_getsize(LIST); ITER++)
+
+// uses "magic" iterator variable
+#define sblist_iter(LIST, PTR) sblist_iter_counter(LIST, __sblist_iterator_name, PTR)
 
 #endif
 
