67f45d9aa75cfdf048afddcd24680253cf9d7445
rofl0r <retnyg@gmx.net>
2011-11-29 18:49:10 +0100

    stringptr_fromfile: make valgrind quiet
    GLibc seemed to access the provided buffer in a way which
    assumed that the nbytes parameter passed to read() is eq.
    to the buffersize, even when the actual filesize was much
    lower.

--- src/stringptr/stringptr_fromfile.c
+++ src/stringptr/stringptr_fromfile.c
@@ -9,6 +9,7 @@ stringptr* stringptr_fromfile(char* filename) {
 	stringptr* buf;
 	size_t size = getfilesize(filename);
 	size_t bufpos = 0;
+	size_t bytesleft;
 	ssize_t bread = 0;
 	if(!size) return NULL;
 	f = open(filename, O_RDONLY);
@@ -17,13 +18,14 @@ stringptr* stringptr_fromfile(char* filename) {
 	if (!buf) goto FEXIT;
 	
 	while(bufpos < size) {
-		if((bread = read(f, buf->ptr + bufpos, 64*1024)) < 0) {
+		bytesleft = size - bufpos;
+		if((bread = read(f, buf->ptr + bufpos, bytesleft > 64*1024 ? 64*1024 : bytesleft)) < 0) {
 			stringptr_free(buf);
 			buf = NULL;
 			goto FEXIT;
 		}
 		bufpos += bread;
-	}	
+	}
 	FEXIT:
 	close(f);
 	return buf;
