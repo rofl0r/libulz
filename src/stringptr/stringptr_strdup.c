9699dbdeaf2653008055d91213d9943ac868c952
rofl0r <retnyg@gmx.net>
2011-11-29 18:44:44 +0100

    make stringptr_strdup always return a valid cstring

--- src/stringptr/stringptr_strdup.c
+++ src/stringptr/stringptr_strdup.c
@@ -1,11 +1,13 @@
 #include <string.h>
+#include <stdlib.h>
 #include "../../include/stringptr.h"
 
 char* stringptr_strdup(stringptr* s) {
-	if(!s || !s->size) return NULL;
+	if(!s) return NULL;
 	char* result;
 	if(!(result = malloc(s->size + 1))) return NULL;
-	memcpy(result, s->ptr, s->size + 1);
+	memcpy(result, s->ptr, s->size);
+	result[s->size] = 0;
 	return result;
 }
 
