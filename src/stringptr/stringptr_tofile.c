6cd8243ce546c859634c70da8cab08f8481706fe
rofl0r <retnyg@gmx.net>
2012-01-03 05:22:01 +0100

    stringptr_tofile: set right permissions

--- src/stringptr/stringptr_tofile.c
+++ src/stringptr/stringptr_tofile.c
@@ -9,7 +9,7 @@ size_t stringptr_tofile(char* filename, stringptr* buffer) {
 	ssize_t ret = 0;
 	int f;
 	size_t togo;
-	if(!filename || !buffer || !buffer->ptr || (f = open(filename, O_WRONLY | O_CREAT | O_TRUNC)) == -1) return 0;
+	if(!filename || !buffer || !buffer->ptr || (f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1) return 0;
 	
 	while(bufpos < buffer->size) {
 		togo = buffer->size - bufpos;
