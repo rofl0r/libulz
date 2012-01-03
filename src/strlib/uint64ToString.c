fd812cc18e64e91355c8d66970c6988eb179974c
rofl0r <retnyg@gmx.net>
2012-01-03 05:40:42 +0100

    proper cast

--- src/strlib/uint64ToString.c
+++ src/strlib/uint64ToString.c
@@ -3,5 +3,5 @@
 // it is in the responsibility of the called to deliver a buffer of suitable length.
 char* uint64ToString(uint64_t number, char* buffer) {
 	if (!buffer) return NULL;
-	return numberToString((int64_t) number, 10, buffer, 0, 0);
+	return numberToString(number, 10, buffer, 0, 0);
 }
--- src/strlib/uintToString.c
+++ src/strlib/uintToString.c
@@ -3,5 +3,5 @@
 // it is in the responsibility of the called to deliver a buffer of suitable length.
 char* uintToString(unsigned int number, char* buffer) {
 	if (!buffer) return NULL;
-	return numberToString((int64_t) number, 10, buffer, 0, 0);
+	return numberToString((uint64_t) number, 10, buffer, 0, 0);
 }
