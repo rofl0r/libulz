24d188127f6539b2e4c67bb6b5accd2ca2cc6558
rofl0r <retnyg@gmx.net>
2013-11-11 23:29:54 +0100

    strtoint*: deal with sign

--- src/strlib/strtoint.c
+++ src/strlib/strtoint.c
@@ -5,9 +5,10 @@ int strtoint(char* str, size_t len) {
 	if(!str || !len) return 0;
 	ssize_t mul = 1;
 	int result = 0;
-	for(i = len - 1; i >= 0; i--) {
+	for(i = len - 1; i >= 0 + (str[0] == '-'); i--) {
 		result += (str[i] - '0') * mul;
 		mul *= 10;
 	}
+	if(str[0] == '-') result *= -1;
 	return result;
 }
