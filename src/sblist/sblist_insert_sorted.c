d877eaffc55a5899cc5237f455bba0cc6d05b149
rofl0r <rofl0r@users.noreply.github.com>
2019-07-03 14:18:50 +0100

    sblist_insert_sorted: make compatible to relaxed compare func
    allows using a compare func that returns values >= 0 resp. <= 0 instead
    of -1/1 like the one below
    
    int intcmp(const void *a, const void *b) {
            int *x = a, *y = b;
            return *x - *y;
    }

--- src/sblist/sblist_insert_sorted.c
+++ src/sblist/sblist_insert_sorted.c
@@ -12,13 +12,13 @@ size_t sblist_insert_sorted(sblist* l, void* o, int (*compar)(const void *, cons
 		void *p = sblist_get(l, c);
 		int r = compar(o, p);
 		if(hi == lo) {
-			if(r == 1) lo++;
+			if(r > 0) lo++;
 	ins:
 			if(sblist_insert(l, o, lo)) return lo;
 			else return (size_t)-1;
 		}
-		if(r == -1) lo = c ? c-1 : 0;
-		else if(r == 1) hi = c+1;
+		if(r < 0) lo = c ? c-1 : 0;
+		else if(r > 0) hi = c+1;
 		else hi = lo = c;
 		if(hi > lo) hi = lo;
 	}
