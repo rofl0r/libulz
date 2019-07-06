f609a5af55630b80a30f4e1426ca5cb2c43602e7
rofl0r <rofl0r@users.noreply.github.com>
2019-07-06 22:37:16 +0100

    add tgilist.h - a list for quick insertion

---
+++ include/tgilist.h
@@ -0,0 +1,83 @@
+#ifndef TGILIST_H
+#define TGILIST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tglist.h"
+
+/* this data structure is nothing more than a thin wrapper around tglist.
+   it's a list providing quick means of insertion: by using a separate
+   index table, the necessary re-shuffling happens only on integer values
+   of user-chosen type, whereas the value list is only ever appended to.
+
+   the usage of this list only makes sense if the value type is larger
+   than the index type (e.g. with a struct).
+
+   as an optimization, deleted items never vanish from the values list,
+   they're simply no longer indexed.
+
+   the index type could be chosen as e.g. short, if it's known that no
+   more than 64K values will ever be added/deleted in total.
+   this means it could be a security risk if it's not under your control.
+   using size_t as the index fixes that, but is naturally slower.
+*/
+
+#define tgilist_impl(NAME, TYPE, IDX_TYPE) \
+	struct NAME { \
+		tglist_impl(, IDX_TYPE) index; \
+		tglist_impl(, TYPE) items; \
+	}
+
+#define tgilist(ID, TYPE, IDX_TYPE) tgilist_impl(tgilist_ ## ID, TYPE, IDX_TYPE)
+#define tgilist_proto tgilist_impl(, void*, size_t)
+
+#define tgilist_getsize(X) tglist_getsize(&(X)->index)
+#define tgilist_empty(X) (tgilist_getsize(X) == 0)
+
+#define tgilist_new() calloc(1, sizeof(tgilist_proto))
+#define tgilist_free(X) do { \
+		tglist_free_items(&(X)->index); \
+		tglist_free_items(&(X)->items); \
+		free(X); } while(0)
+
+#define tgilist_init(X) memset(X, 0, sizeof(*(X)))
+#define tgilist_free_items(X) do {\
+		tglist_free_items(&(X)->index); \
+		tglist_free_items(&(X)->items); \
+		} while(0)
+
+#define tgilist_free_values(X) tglist_free_values(&(X)->items)
+
+#define tgilist_get(X, POS) ((X)->items.items[(X)->index.items[POS]])
+
+#define tgilist_set(X, ITEM, POS) ((tgilist_get(X, POS) = ITEM), 1)
+
+#define tgilist_add(X, ITEM) ( \
+	!tglist_add( &(X)->index, tglist_getsize(&(X)->items)) ? \
+	0 : ( \
+		(tglist_add( &(X)->items, ITEM)) ? 1 : \
+			(tglist_delete(&(X)->index, tglist_getsize(&(X)->index)-1),  0) \
+			) \
+	)
+
+/* only index items are only ever deleted before freeing the list */
+#define tgilist_delete(X, POS) \
+	tglist_delete(&(X)->index, POS)
+
+#define tgilist_insert(X, ITEM, POS) ( \
+	!(tglist_add(&(X)->items, ITEM)) ? \
+	0 : ( \
+		tglist_insert(&(X)->index, tglist_getsize(&(X)->items)-1, POS) ? 1 : \
+		( --((X)->items.count), 0) \
+		) \
+	)
+
+#define tgilist_itemsize(X) tglist_itemsize(&(X)->items)
+
+#endif
+
---
+++ tests/tgilist_insert_test.c
@@ -0,0 +1,149 @@
+#include "../include/tgilist.h"
+#include "../include/macros.h"
+#include <assert.h>
+
+static int intcmp(const void *vp1, const void* vp2) {
+	const int* a = vp1;
+	const int* b = vp2;
+	return *a - *b;
+}
+
+static int mystrstrcmp(const void *vp1, const void* vp2) {
+	const char * const *a = vp1;
+	const char * const *b = vp2;
+	return strcmp(*a, *b);
+}
+
+int strtest() {
+	tgilist(str, char*, unsigned short) l_buf, *l = &l_buf;
+	tgilist_init(l);
+	const char* org[] = {
+		"Lorem ipsum dolor sit amet, ",
+		"consectetur adipiscing elit, ",
+		"sed do eiusmod tempor incididunt ",
+		"ut labore et dolore magna aliqua. ",
+		"Ut enim ad minim veniam, ",
+		"quis nostrud exercitation ullamco laboris ",
+		"nisi ut aliquip ex ea commodo consequat. ",
+		"Duis aute irure dolor in reprehenderit ",
+		"in voluptate velit esse cillum dolore ",
+		"eu fugiat nulla pariatur. ",
+		"Excepteur sint occaecat cupidatat non proident, ",
+		"sunt in culpa qui officia deserunt mollit ",
+		"anim id est laborum.",
+		0
+	};
+	size_t i;
+	for(i = 0; org[i]; i++) {
+		tgilist_add(l, strdup(org[i]));
+	}
+	for(i = 0; org[i]; i++)
+		assert(!strcmp(org[i], tgilist_get(l, i)));
+
+	tgilist_insert(l, strdup(org[7]), 0);
+	tgilist_insert(l, strdup(org[7]), 0);
+
+	assert(tgilist_getsize(l) == ARRAY_SIZE(org)-1+2);
+
+	assert(!strncmp("Duis", tgilist_get(l, 0), 4));
+	assert(!strncmp("Duis", tgilist_get(l, 1), 4));
+
+	tgilist_delete(l, 1);
+	tgilist_delete(l, 0);
+
+	assert( strncmp("Duis", tgilist_get(l, 0), 4));
+	assert( tgilist_getsize(l) == ARRAY_SIZE(org)-1);
+
+	for(i = 0; org[i]; i++)
+		assert(!strcmp(org[i], tgilist_get(l, i)));
+
+	tgilist_delete(l, 1);
+	assert(!strcmp(org[0], tgilist_get(l, 0)));
+	assert(!strcmp(org[2], tgilist_get(l, 1)));
+
+	assert( tgilist_getsize(l) == ARRAY_SIZE(org)-1-1);
+
+	tgilist_free_values(l);
+	tgilist_free_items(l);
+	return 0;
+}
+
+int main() {
+	int i = 0;
+	tgilist(_, int, short) *l = tgilist_new();
+
+	assert(tgilist_getsize(l) == 0);
+	tgilist_add(l, i);
+	assert(tgilist_getsize(l) == 1);
+	i++;
+	tgilist_add(l, i);
+	assert(tgilist_getsize(l) == 2);
+	i++;
+	tgilist_add(l, i);
+	assert(tgilist_getsize(l) == 3);
+	i++;
+
+	/* i is 3 at this point */
+	tgilist_insert(l, i, 2);
+	assert(tgilist_getsize(l) == 4);
+	i++;
+	assert(tgilist_get(l, 0) == 0);
+	assert(tgilist_get(l, 1) == 1);
+	assert(tgilist_get(l, 2) == 3);
+	assert(tgilist_get(l, 3) == 2);
+
+	assert(tgilist_getsize(l) == 4);
+
+	tgilist_free(l);
+
+	l = tgilist_new();
+	assert(tgilist_getsize(l) == 0);
+
+	i = 0;
+	for(i=0; i<16384; i++) {
+		tgilist_insert(l, i, i);
+		assert(tgilist_getsize(l) == i+1);
+	}
+
+	assert(tgilist_get(l, 0) == 0);
+	assert(tgilist_get(l, 1) == 1);
+	assert(tgilist_get(l, 2) == 2);
+	assert(tgilist_get(l, 3) == 3);
+
+	assert(tgilist_get(l, 16000) == 16000);
+
+	i = 3;
+	assert(tgilist_insert(l, i, 3));
+	assert(tgilist_get(l, 3) == 3);
+	assert(tgilist_get(l, 4) == 3);
+
+	assert(tgilist_get(l, 16000) == 15999);
+
+
+
+
+	l = tgilist_new();
+	assert(tgilist_getsize(l) == 0);
+
+	i = 0;
+
+	tgilist_insert(l, i, 0);
+	assert(tgilist_getsize(l) == 1);
+	i++;
+	tgilist_insert(l, i, 0);
+	assert(tgilist_getsize(l) == 2);
+	i++;
+	tgilist_insert(l, i, 0);
+	assert(tgilist_getsize(l) == 3);
+	i++;
+	tgilist_insert(l, i, 0);
+	assert(tgilist_getsize(l) == 4);
+	i++;
+
+	assert(tgilist_get(l, 0) == 3);
+	assert(tgilist_get(l, 1) == 2);
+	assert(tgilist_get(l, 2) == 1);
+	assert(tgilist_get(l, 3) == 0);
+
+	return strtest();
+}
