17c166b71dd1964149597a85d5c0e83f5e71e098
rofl0r <rofl0r@users.noreply.github.com>
2019-07-07 03:52:39 +0100

    add hbmap, a hashmap based on bmap

---
+++ include/hbmap.h
@@ -0,0 +1,120 @@
+#ifndef HBMAP_H
+#define HBMAP_H
+
+/* this is a hashmap using a fixed number of buckets,
+   which in turn are of type bmap. this combines the advantages of both
+   approaches.
+   limitations: max no of buckets and items per bucket is 2^32-1 each.
+   speed is almost identical to khash with small number of items per
+   bucket. with 100.000 items it's about 15% slower.
+
+   unlike bmap, _find(), insert(), etc return an iterator instead of indices.
+   the iterator needs to be used for e.g. _getkey(), etc.
+*/
+
+#include "bmap.h"
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h> /* ssize_t */
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#endif
+
+typedef uint64_t hbmap_iter;
+
+#define hbmap_impl(NAME, KEYTYPE, VALTYPE, NUMBUCKETS) \
+struct NAME { \
+	unsigned (*hash_func)(const KEYTYPE); \
+	union { \
+		hbmap_iter it; \
+	} tmp; \
+	bmap_impl(, KEYTYPE, VALTYPE) buckets[NUMBUCKETS]; \
+}
+
+#define hbmap(ID, KEYTYPE, VALTYPE, NUMBUCKETS) \
+	hbmap_impl(hbmap_ ## ID, KEYTYPE, VALTYPE, NUMBUCKETS)
+
+#define hbmap_proto(NUMBUCKETS) \
+	hbmap_impl(, void*, void*, NUMBUCKETS)
+
+/* initialization */
+/* bmap_compare_func is a typical compare function used for qsort, etc such as strcmp
+ */
+#define hbmap_init(X, COMPAREFUNC, HASHFUNC) do{\
+	memset(X, 0, sizeof(*(X))); \
+	size_t i; for(i=0; i<ARRAY_SIZE((X)->buckets); ++i) \
+		(X)->buckets[i].compare = COMPAREFUNC; \
+	(X)->hash_func = HASHFUNC; \
+	} while(0)
+
+static inline void* hbmap_new(bmap_compare_func fn, void* hash_func, size_t numbuckets) {
+	hbmap_proto(numbuckets) *nyu = malloc(sizeof(hbmap_proto(numbuckets)));
+	if(nyu) hbmap_init(nyu, fn, hash_func);
+	return nyu;
+}
+
+/* destruction */
+/* freeflags:
+  0: free only internal mem
+  1: 0+free all keys,
+  2: 0+free all values,
+  3: 0+free both
+*/
+#define hbmap_fini(X, FREEFLAGS) do { \
+	size_t i; for(i=0; i<ARRAY_SIZE((X)->buckets); ++i) \
+		{ bmap_fini(&(X)->buckets[i], FREEFLAGS); } \
+} while(0)
+
+#define hbmap_iter_bucket(I) ( (I) >> 32)
+#define hbmap_iter_index(I)  ( (I) & 0xffffffff )
+#define hbmap_iter_makebucket(I) ( (I) << 32)
+
+#define hbmap_getkey(X, ITER) \
+	bmap_getkey(&(X)->buckets[hbmap_iter_bucket(ITER)], hbmap_iter_index(ITER))
+
+#define hbmap_getval(X, ITER) \
+	bmap_getval(&(X)->buckets[hbmap_iter_bucket(ITER)], hbmap_iter_index(ITER))
+
+#define hbmap_setvalue(X, VAL, ITER) \
+	bmap_setvalue(&(X)->buckets[hbmap_iter_bucket(ITER)], VAL, hbmap_iter_index(ITER))
+
+#define hbmap_getkeysize(X) (bmap_getkeysize(&(X)->buckets[0]))
+#define hbmap_getvalsize(X) (bmap_getvalsize(&(X)->buckets[0]))
+#define hbmap_getbucketcount(X) ARRAY_SIZE((X)->buckets)
+
+#define hbmap_buckindex(X, KEY) \
+	( (X)->hash_func(KEY) % hbmap_getbucketcount(X) )
+
+#define hbmap_find(X, KEY) ( \
+	( (X)->tmp.it = hbmap_iter_makebucket(hbmap_buckindex(X, KEY) ) ), \
+	((X)->tmp.it |= (int64_t) bmap_find(&(X)->buckets[ hbmap_iter_bucket((X)->tmp.it) ], KEY)), \
+	(X)->tmp.it)
+
+#define hbmap_contains(X, KEY) (hbmap_find(X, KEY) != (hbmap_iter)-1)
+
+/* unlike hbmap_getkey/val with index, this returns a pointer-to-item, or NULL */
+#define hbmap_get(X, KEY) ( \
+	( hbmap_find(X, KEY) == (hbmap_iter) -1 ) ? 0 : &hbmap_getval(X, (X)->tmp.it) \
+	)
+
+/* same as hbmap_insert, but inserts blindly without checking for existing items.
+   this is faster and can be used when it's impossible that duplicate
+   items are added */
+#define hbmap_insert_nocheck(X, KEY, VAL) ( \
+	( (X)->tmp.it = hbmap_iter_makebucket(hbmap_buckindex(X, KEY) ) ), \
+	((X)->tmp.it |= (int64_t) bmap_insert_nocheck(&(X)->buckets[hbmap_iter_bucket((X)->tmp.it)], KEY, VAL)), \
+	(X)->tmp.it)
+
+/* insert item into mapping, overwriting existing items with the same key */
+/* return index of new item, or -1. overwrites existing items. */
+#define hbmap_insert(X, KEY, VAL) ( \
+		( hbmap_find(X, KEY) == (hbmap_iter) -1 ) ? hbmap_insert_nocheck(X, KEY, VAL) : \
+		( hbmap_setvalue(X, VAL, (X)->tmp.it), (X)->tmp.it ) \
+	)
+
+#define hbmap_delete(X, ITER) ( \
+	bmap_delete(&(X)->buckets[hbmap_iter_bucket(ITER)], hbmap_iter_index(ITER)), 1)
+
+#endif
---
+++ tests/hbmap_bench.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#define N_ITEMS 1000000
+#define N_SEEKS 10000
+#define N_LOOPS 20
+
+static int strstrcmp(const void *a, const void *b) {
+	const char * const *x = a;
+	const char * const *y = b;
+	return strcmp(*x, *y);
+}
+
+
+#ifdef USE_KHASH
+
+#include "../../../../klib/khash.h"
+
+KHASH_MAP_INIT_STR(km, int)
+
+static int* get_kh_value(khash_t(km) *b, const char *name) {
+        khint_t k = kh_get(km, b, name);
+        if(k == kh_end(b)) return 0;
+        return &kh_value(b, k);
+}
+
+static int insert_kh(khash_t(km) *b, const char *name, int* m) {
+        int absent;
+        khint_t k = kh_put(km, b, name, &absent);
+        if (!absent) {
+        }
+        kh_value(b, k) = *m;
+        return !absent;
+}
+
+
+int test_khash() {
+	khash_t(km) *b;
+	b = kh_init(km);
+
+	char buf[64];
+	int i;
+	for(i=0; i<N_ITEMS; i++) {
+		sprintf(buf, "%d", i);
+		const char *key = strdup(buf);
+		insert_kh(b, key, &i);
+	}
+	for(i=0; i<N_ITEMS; i++) {
+		sprintf(buf, "%d", i);
+		const char *key = buf;
+		assert(get_kh_value(b, key));
+	}
+
+	srand(0);
+	int res = 0;
+	for(i=0; i<N_SEEKS; i++) {
+		int val = rand()%N_ITEMS;
+		sprintf(buf, "%d", val);
+		const char *key = buf;
+		res += *(int*)get_kh_value(b, key);
+	}
+	return res;
+}
+#else
+
+#include "../include/hbmap.h"
+
+static inline unsigned __ac_X31_hash_string(const char *s)
+{
+	unsigned h = (unsigned)*s;
+	if (h) for (++s ; *s; ++s) h = (h << 5) - h + (unsigned)*s;
+	return h;
+}
+
+
+int test_bmap() {
+	hbmap(_, char*, int, 256) b_b, *b = &b_b;
+	hbmap_init(b, strstrcmp, __ac_X31_hash_string);
+
+	char buf[64];
+	int i;
+	for(i=0; i<N_ITEMS; i++) {
+		sprintf(buf, "%d", i);
+		const char *key = strdup(buf);
+		hbmap_insert_nocheck(b, key, i);
+	}
+
+	// check that hbmap_insert works, too */
+	i = 0; sprintf(buf, "0");
+	const char *key = strdup(buf);
+	//assert(hbmap_insert(b,  key, i) == 0);
+
+	for(i=0; i<N_ITEMS; i++) {
+		sprintf(buf, "%d", i);
+		const char *key = buf;
+		assert(hbmap_contains(b, key));
+	}
+
+	srand(0);
+	int res = 0;
+	for(i=0; i<N_SEEKS; i++) {
+		int val = rand()%N_ITEMS;
+		sprintf(buf, "%d", val);
+		const char *key = buf;
+		res += *hbmap_get(b, key);
+	}
+
+	hbmap_fini(b, 1);
+	return res;
+}
+
+
+static int intcmp(const void *a, const void *b) {
+	int const *x = a;
+	int const *y = b;
+	return *x - *y;
+}
+
+static inline unsigned int_hash(int x) { return x; }
+
+int test_bmap_int() {
+	hbmap(_, int, int, 256) b_b, *b = &b_b;
+	hbmap_init(b, intcmp, int_hash);
+
+	int i;
+	for(i=0; i<N_ITEMS; i++) {
+		hbmap_insert(b, i, i);
+	}
+	for(i=0; i<N_ITEMS; i++) {
+		assert(hbmap_contains(b, i));
+	}
+
+	srand(0);
+	int res = 0;
+	for(i=0; i<N_SEEKS; i++) {
+		int val = rand()%N_ITEMS;
+		res += *hbmap_get(b, val);
+	}
+	return res;
+}
+
+#endif
+
+int main(int argc, char**argv) {
+	int i;
+#ifdef USE_KHASH
+
+	if(argc > 1 && !strcmp(argv[1], "khash"))
+		for(i=0; i< N_LOOPS; i++)
+			printf("%d\n", test_khash());
+#else
+//	else
+		for(i=0; i< N_LOOPS; i++)
+			printf("%d\n", test_bmap());
+#endif
+
+}
