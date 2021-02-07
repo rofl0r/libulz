011499cb91064c9ed54e6e9f30bf782baf478bd4
rofl0r <rofl0r@users.noreply.github.com>
2021-02-07 23:01:26 +0000

    proclib.h: first try at proper doxygen-style documentation

--- include/proclib.h
+++ include/proclib.h
@@ -1,6 +1,14 @@
 #ifndef PROCLIB_H
 #define PROCLIB_H
 
+/** @file
+ * libulz process management
+ */
+
+/**
+ * background calling process.
+ * detaches from foreground terminal.
+ */
 void daemonize(void);
 
 #include <spawn.h>
@@ -13,9 +21,21 @@ struct process {
 	posix_spawn_file_actions_t fa;
 };
 
-/* return 0 on success, -1 on error. in error case, you can query errno. */
+/**
+ * run a process.
+ * @param p  pointer to process struct. initialize manually with desired fds
+ *  before usage.
+ * @param argv  argv parameter to be passed to the executed process.
+ *  requires the last element to be a null pointer.
+ * @return 0 on success, -1 on error. in error case, you can query errno. */
 int process_open(struct process *p, char* const argv[]);
-/* closes pipes, waits for process to exit, returns exitcode */
+
+/**
+ * terminate a process started with process_open.
+ * closes pipes, waits for process to exit, returns exitcode.
+ * @param p  process handle which was initialized with process_open.
+ * @return process exitcode.
+ */
 int process_close(struct process *p);
 
 #pragma RcB2 DEP "../src/proclib/*.c"
